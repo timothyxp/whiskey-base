9abb04cd610966db23e674900e5571cdbe112754
Timofey Smirnov <32017414+timothyxp@users.noreply.github.com>
2025-01-19 20:00:54 +0300

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🥃 Whiskey Base 2.0",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
